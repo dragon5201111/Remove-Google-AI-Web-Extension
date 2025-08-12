79024e90e2db5dc919c2cae5208fefb7109d2c47
evoexo <dragon520111back@gmail.com>
2025-08-12 09:02:03 -0400

    Removed dfs for finding related question pairs. Simplified code, and added mutation observer.

--- content.js
+++ content.js
@@ -1,93 +1,50 @@
-// TODO : Use mutation observer to respond to changes to the document in real-time
+const patterns = [
+    /ai overview/i
+]
 
-// TODO: Differentiate between image and ai overview header.
-const headerAiOverviewDiv = 
-    document.querySelector("div.bzXtMb.M8OgIe.dRpWwb") ? document.getElementById("Odp5De") : null
-removeNodeIfExists(headerAiOverviewDiv)
+// Header is only loaded on the first search page; don't need to respond to dom changes
+const headerAiOverviewDiv = document.getElementById("Odp5De")
+if (headerAiOverviewDiv){
+    if (innerHTMLMatchesPattern(headerAiOverviewDiv.innerHTML)){
+        hideElement(headerAiOverviewDiv)
+    }
+}
 
-const aiOverviewBelowQueries = document.querySelector("div.YzCcne")
-removeNodeIfExists(aiOverviewBelowQueries)
 
-const aiModeDiv = document.querySelector("div.olrp5b")
-removeNodeIfExists(aiModeDiv)
+const documentObserver = new MutationObserver(()=>{
+    const relatedQuestionPairs = document.querySelectorAll("div.related-question-pair")
+    relatedQuestionPairs.forEach((questionPair)=>{
+        if (hasBeenVisited(questionPair)){
+            return
+        }
 
-const aiModeButton = document.querySelector("button.plR5qb")
-removeNodeIfExists(aiModeButton)
-
-// TODO: Remove dfs (on click listener) and integrate this solution for removing ai overview from people also ask
-// document.querySelectorAll("div.related-question-pair").forEach((el)=>{
-//     if ((/ai overview/i).test(el.innerHTML)){
-//         el.remove()
-//     }
-// })
-
-const peopleAlsoAskDiv = document.querySelector("div.Wt5Tfe")
-const questionPairDiv = peopleAlsoAskDiv.getElementsByClassName("LQCGqc")[0]
-questionPairDiv.addEventListener("click", ()=>{
-    deleteAiQuestionPairs(questionPairDiv)
-    markChildrenAsVisited(questionPairDiv)
+        if (innerHTMLMatchesPattern(questionPair.innerHTML)){
+            hideElement(questionPair)
+        }
+        
+        markAsVisited(questionPair)
+    })
 })
 
-function markChildrenAsVisited(div){
-    let children = div.children
-    for (let i = 0; i < children.length; i++){
-        children[i].setAttribute("visited", "")
-    }
+function hasBeenVisited(element){
+    return element.hasAttribute("visited")
 }
 
-function deleteAiQuestionPairs(div){
-    let childrenToDelete = getQuestionPairsToDelete(div)
-
-    childrenToDelete.forEach((element)=>{
-        element.remove()
-    })
+function innerHTMLMatchesPattern(innerHTML){
+    return patterns.some(pattern => pattern.test(innerHTML))
 }
 
-function getQuestionPairsToDelete(div){
-    let children = div.children
-    let childrenToDelete = []
-
-
-    for (let i = 0; i < children.length; i++){
-        let child = children[i]
-        if (childHasBeenVisited(child)){
-            continue
-        }
-
-        if (aiOverviewPairFound(child)){
-            childrenToDelete.push(child)
-        }
-    }
-
-    return childrenToDelete
+function hideElement(element){
+    element.style.display = "none"
 }
 
-function childHasBeenVisited(child){
-    return child.hasAttribute("visited")
+function markAsVisited(element){
+    element.setAttribute("visited", "")
 }
 
-function aiOverviewPairFound(div){
-    stack = []
-    stack.push(div)
-
-    while (stack.length){
-        element = stack.pop()
-
-        if (element.tagName == "STRONG" && element.textContent == "AI Overview"){
-            return true
-        }
-
-        let children = element.children
-        for (let i = 0; i < children.length; i++){
-            stack.push(children[i])
-        }
-    }
-
-    return false
+const observerConfig = {
+    childList : true,
+    subtree : true
 }
 
-function removeNodeIfExists(node){
-    if (node){
-        node.remove()
-    }
-}
+documentObserver.observe(document, observerConfig)
