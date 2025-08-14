5df6921a11f5757bdc972b76682c87e364af3d06
evoexo <dragon520111back@gmail.com>
2025-08-14 19:57:44 -0400

    Encapsulated the hiding of elements into a static class.

--- content.js
+++ content.js
@@ -2,48 +2,24 @@ const patterns = [
     /ai overview/i
 ]
 
-const aiMode = document.querySelector("div.olrp5b")
-if (aiMode){
-    hideElement(aiMode)
-}
+const aiModeTab = document.querySelector("div.olrp5b")
+ElementHider.hide(aiModeTab, patterns)
 
 const aiOverviewHeader = document.getElementById("Odp5De")
-if (aiOverviewHeader){
-    if (innerHTMLMatchesPattern(aiOverviewHeader.innerHTML)){
-        hideElement(aiOverviewHeader)
-    }
-}
+ElementHider.hide(aiOverviewHeader, patterns)
 
 const documentObserver = new MutationObserver(()=>{
+    const aiOverviewElements = document.querySelectorAll("[data-mcpr]")
+    aiOverviewElements.forEach((element)=>{
+        ElementHider.hide(element, patterns)
+    })
+
     const relatedQuestionPairs = document.querySelectorAll("div.related-question-pair")
     relatedQuestionPairs.forEach((questionPair)=>{
-        if (hasBeenVisited(questionPair)){
-            return
-        }
-
-        if (innerHTMLMatchesPattern(questionPair.innerHTML)){
-            hideElement(questionPair)
-        }
-        
-        markAsVisited(questionPair)
+        ElementHider.hide(questionPair, patterns)
     })
 })
 
-function hasBeenVisited(element){
-    return element.hasAttribute("visited")
-}
-
-function innerHTMLMatchesPattern(innerHTML){
-    return patterns.some(pattern => pattern.test(innerHTML))
-}
-
-function hideElement(element){
-    element.style.display = "none"
-}
-
-function markAsVisited(element){
-    element.setAttribute("visited", "")
-}
 
 const observerConfig = {
     childList : true,
---
+++ elementHider.js
@@ -0,0 +1,34 @@
+class ElementHider{
+    static HIDE_ATTRIBUTE = "hidden"
+
+    static hide(element, regexPatterns){
+        if (!element){
+            return
+        }
+
+        if (ElementHider.hasBeenHidden(element)) {
+            return;
+        }
+
+        if (ElementHider.innerHTMLMatchesAPattern(element, regexPatterns)) {
+            ElementHider.makeElementInvisible(element);
+            ElementHider.markElementAsHidden(element);
+        }
+    }
+
+    static hasBeenHidden(element) {
+        return element.hasAttribute(ElementHider.HIDE_ATTRIBUTE);
+    }
+
+    static innerHTMLMatchesAPattern(element, regexPatterns) {
+        return regexPatterns.some(pattern => pattern.test(element.innerHTML));
+    }
+
+    static makeElementInvisible(element) {
+        element.style.display = "none";
+    }
+
+    static markElementAsHidden(element) {
+        element.setAttribute(ElementHider.HIDE_ATTRIBUTE, "");
+    }
+}
